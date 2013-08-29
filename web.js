c47a5716a26c997a631182ef41471a3205913aad
jtopal <jtopal@gmail.com>
2013-08-29 05:46:23 +0000

    files

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ var buf = new Buffer (fs.readFileSync('contact.html'), 'utf-8');
 
  });
 
-app.use(express.static(__dirname+'/public'));
+app.configure(function(){app.use(express.static(__dirname + '/css'));});
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
